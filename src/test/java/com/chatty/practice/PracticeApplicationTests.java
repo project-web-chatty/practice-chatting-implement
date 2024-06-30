69e32ce6f3a612e84fb13a2b12f325053c259cfb
suhyeonOh <sh020119@snu.ac.kr>
2024-06-30 12:22:16 +0900

    init/project init

---
+++ src/test/java/com/chatty/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatty.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
